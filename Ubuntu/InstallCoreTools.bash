f9272bbd7da7e74d612176c77a1f9f88348086e3
daNutz <dan.durden@outlook.com>
2022-07-25 18:30:38 +0100

    .

---
+++ Ubuntu/InstallCoreTools.bash
@@ -0,0 +1,19 @@
+# Install Core Tools
+
+## Firewall & Configure
+sudo apt-get install ufw && sudo ufw allow ssh && sudo ufw enable
+
+## System Monitor
+sudo apt-get install glances
+
+## HardwareInfo
+sudo apt-get install dmidecode
+
+## Disk Performance Tools
+sudo apt-get install atop
+sudo apt-get install htop
+sudo apt-get install iotop
+sudo apt-get install bashtop
+
+## 7zip
+sudo apt-get install p7zip-full
