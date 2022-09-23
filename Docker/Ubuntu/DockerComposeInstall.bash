e4edfb49131af39d1a774abeec9f343b7a9a9e78
daNutz <dan.durden@outlook.com>
2022-09-23 15:13:12 +0100

    Update DockerComposeInstall.bash

--- Docker/Ubuntu/DockerComposeInstall.bash
+++ Docker/Ubuntu/DockerComposeInstall.bash
@@ -2,4 +2,4 @@ docker 01
 docker compose -f /mnt/ssd1/docker/docker-compose/docker-compose.yml up -d
 
 docker 02
-docker compose -f /mnt/hdd-1tb/docker/docker-compose/docker-compose.yml up -d
+docker compose -f /mnt/ssd1/docker/docker-compose/docker-compose.yml up -d
