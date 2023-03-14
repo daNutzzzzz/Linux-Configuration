28066b93da34c074f1f037e9dada8b174d100184
daNutz <dan.durden@outlook.com>
2023-03-14 22:08:47 +0000

    Update InstallCoreTools.bash

--- Ubuntu/InstallCoreTools.bash
+++ Ubuntu/InstallCoreTools.bash
@@ -1,32 +1,26 @@
 # Install Core Tools
-
-## Firewall & Configure
-sudo apt-get -y install ufw && sudo ufw allow ssh && sudo ufw enable
-
+sudo apt-get -y install \
 ## System Monitor
-sudo apt-get -y install glances
-
+   glances \
 ## HardwareInfo
-sudo apt-get -y install dmidecode
-
+   dmidecode \
 ## Disk Management Tools
-sudo apt-get -y install parted
-
-# Network Management Tools
-sudo apt-get -y install iputils-ping
-sudo apt-get -y install net-tools  
-
+   parted \
+## Network Management Tools
+   iputils-ping \
+   net-tools \
 ## Disk Performance Tools
-sudo apt-get -y install atop
-sudo apt-get -y install htop
-sudo apt-get -y install iotop
-sudo apt-get -y install bashtop
-
-# Install File Editor
-sudo apt-get -y install nano
-
-# storage
-sudo apt-get -y install duf
-
+   atop \
+   htop \
+   iotop \
+   bashtop \
+## Install File Editor
+   nano \
+## storage
+   duf \
 ## 7zip
-sudo apt-get -y install p7zip-full
+   p7zip-full \
+## Firewall & Configure
+   ufw \
+sudo ufw allow ssh && sudo ufw enable
+
