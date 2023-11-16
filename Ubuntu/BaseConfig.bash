501de5f04ea0e6b3c1a8996860d8ee04b82a56d5
daNutz <dan.durden@outlook.com>
2023-11-16 19:55:48 +0000

    Update BaseConfig.bash

--- Ubuntu/BaseConfig.bash
+++ Ubuntu/BaseConfig.bash
@@ -14,3 +14,7 @@ sudo usermod -aG sudo docker
 
 # Verify User Belongs to Sudo Group
 groups docker
+
+# QEMU Guest Agent: https://pve.proxmox.com/wiki/Qemu-guest-agent
+sudo apt-get install qemu-guest-agent
+systemctl start qemu-guest-agent
