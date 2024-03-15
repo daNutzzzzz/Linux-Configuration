3968f155da555ba0a985fad25cd2c6a1f2fdf15d
daNutz <dan.durden@outlook.com>
2024-03-15 13:47:57 +0000

    GDU

--- Ubuntu/InstallCoreTools.bash
+++ Ubuntu/InstallCoreTools.bash
@@ -24,3 +24,7 @@ sudo apt-get -y install \
    ufw \
 sudo ufw allow ssh && sudo ufw enable
 
+# GDU
+curl -L https://github.com/dundee/gdu/releases/latest/download/gdu_linux_amd64.tgz | tar xz \
+chmod +x gdu_linux_amd64 \
+mv gdu_linux_amd64 /usr/bin/gdu
