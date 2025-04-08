975a52405d2b1fda2d036346e0dbc99697b18e31
daNutz <89404081+daNutzzzzz@users.noreply.github.com>
2025-04-08 21:23:58 +0100

    Create Docker-SetDefaultComposeLocation.sh

---
+++ Docker/Ubuntu/Docker-SetDefaultComposeLocation.sh
@@ -0,0 +1,7 @@
+export COMPOSE_FILE=/mnt/ssd1/docker/docker-compose/docker-compose.yml
+
+source ~/.bashrc  # or source ~/.zshrc
+
+
+nano /etc/environment
+COMPOSE_FILE=/mnt/ssd1/docker/docker-compose/docker-compose.yml
